d6304ec98426a1933da44cec741b4a59214771ae
RushdiNura1 <rushdinurafita@gmail.com>
2026-05-17 08:19:52 +0300

    check this

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://hr-report-backend.onrender.com/api",
+  // baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
